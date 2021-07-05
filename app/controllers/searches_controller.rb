3479763b0a6bf4e64b8d0cafbd9f27300ffbe87f
kgdskc <wajima0503@gmail.com>
2021-07-05 07:53:38 +0000

    [Finish] 5-5

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,34 @@
+class SearchesController < ApplicationController
+
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
+
+  private
+  def search_for(model, content, method)
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content)
+      elsif method == 'partial'
+        User.where('name LIKE ?', '%'+content+'%')
+      elsif method == 'forward_match'
+        User.where("name LIKE?","#{content}%")
+      elsif method == 'backward_match'
+        User.where("name LIKE?","%#{content}")
+      end
+    elsif model == 'book'
+      if method == 'perfect'
+        Book.where(title: content)
+      elsif method == 'partial'
+        Book.where('title LIKE ?', '%'+content+'%')
+      elsif method == 'forward_match'
+        Book.where("title LIKE?","#{content}%")
+      elsif method == 'backward_match'
+        Book.where("title LIKE?","%#{content}")
+      end
+    end
+  end
+end
